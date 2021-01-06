45c02b1f3b1add0e1e0616157fd36968f83ca6a6
Chuang, Benjamin <bschuang@gatech.edu>
2021-01-06 14:54:08 -0500

    fix indents

--- scripts/WebcamVideoStream.py
+++ scripts/WebcamVideoStream.py
@@ -5,9 +5,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 
 class WebcamVideoStream:
-	def __init__(self, src=0):
-		# initialize the video camera stream and read the first frame
-		# from the stream
+    def __init__(self, src=0):
         self.camera = PiCamera()
         self.camera.resolution = (640, 480)
         self.camera.framerate = 32
@@ -17,26 +15,23 @@ class WebcamVideoStream:
 		#(self.grabbed, self.frame) = self.stream.read()
 		# initialize the variable used to indicate if the thread should
 		# be stopped
-		self.stopped = False
-	def start(self):
-		# start the thread to read frames from the video stream
-		Thread(target=self.update, args=()).start()
-		return self
-	def update(self):
+        self.stopped = False
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+    def update(self):
         for frame in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
-	        # grab the raw NumPy array representing the image, then initialize the timestamp
-	        # and occupied/unoccupied text
-	        self.frame = frame.array
+            self.frame = frame.array
 		# keep looping infinitely until the thread is stopped
-		""" while True:
-			# if the thread indicator variable is set, stop the thread
-			if self.stopped:
-				return
-			# otherwise, read the next frame from the stream
-			(self.grabbed, self.frame) = self.stream.read() """
-	def read(self):
-		# return the frame most recently read
-		return self.frame
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
+        """ while True:
+        # if the thread indicator variable is set, stop the thread
+        # if self.stopped:
+        # return
+        # # otherwise, read the next frame from the stream
+        # (self.grabbed, self.frame) = self.stream.read() """
+    def read(self):
+        return self.frame
+    def stop(self):
+    # indicate that the thread should be stopped
+        self.stopped = True
