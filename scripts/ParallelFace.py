c5453dfeb3fdd6bc13aa65246d177c7311dba63d
Chuang, Benjamin <bschuang@gatech.edu>
2021-01-06 13:06:22 -0500

    threading camera input

--- scripts/ParallelFace.py
+++ scripts/ParallelFace.py
@@ -6,9 +6,11 @@ import numpy as np
 import uuid
 import glob
 import json
+from WebcamVideoStream import WebcamVideoStream
 
 def fParallelFace(q):
-    video_capture = cv2.VideoCapture(0)
+    #video_capture = cv2.VideoCapture(0)
+    vs = WebcamVideoStream(src=0).start()
 
     # Create arrays of known face encodings and their names
     known_face_encodings = []
@@ -35,13 +37,13 @@ def fParallelFace(q):
 
     newUser = None
     while True:
-        ret, frame = video_capture.read()
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        if process_this_frame % 20 == 0:
+            process_this_frame = 0
+            frame = vs.read()
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame[:, :, ::-1]
 
-        # Only process every other frame of video to save time
-        if process_this_frame % 10 == 0:
-
+            # Only process every other frame of video to save time
             # QR Code reading
             try:
                 qrText = qrCodeDetector.detectAndDecode(frame)[0]
@@ -96,31 +98,34 @@ def fParallelFace(q):
 
                 q.put({'process': 'face', 'args': name})
 
-        process_this_frame = (process_this_frame % 10) + 1
+        process_this_frame += 1
         
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+        if True:
+            # Display the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
 
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-        # Display the resulting image
-        cv2.imshow('Video', frame)
+            # Display the resulting image
+            cv2.imshow('Video', frame)
 
         # Hit 'q' on the keyboard to quit!
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     # Release handle to the webcam
-    video_capture.release()
+    #video_capture.release()
+    vs.stop()
     cv2.destroyAllWindows()
+    
